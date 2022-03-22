f517fd640b731ba39546fd9b0721f812288b752c
burcu25 <brckork@gmail.com>
2022-03-22 23:57:12 +0300

    css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar nav justify-content-center bg-dark ">
         <ul>
             <li><a href="#home">ABOUT US</a></li>
             <li><a href="#news">FOR YOU</a></li>
             <li><a href="#contact">SERVICES</a></li>
-            <li><a href="#about">BLOG</a></li>
-            <li><a href="#about">VLOG</a></li>
-            <li><a href="#about">CONTACT</a></li>
+            <li ><a href="#about">BLOG</a></li>
+            <li ><a href="#about">VLOG</a></li>
+            <li ><a href="#about">CONTACT</a></li>
         </ul>
 
       
