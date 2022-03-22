40d3dd39bc74ff24ed7fbecba9b388f10d8011d9
burcu25 <brckork@gmail.com>
2022-03-23 00:01:20 +0300

    href

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <div className="navbar nav justify-content-center bg-dark ">
         <ul>
-            <li><a href="#home">ABOUT US</a></li>
-            <li><a href="#news">FOR YOU</a></li>
-            <li><a href="#contact">SERVICES</a></li>
-            <li ><a href="#about">BLOG</a></li>
-            <li ><a href="#about">VLOG</a></li>
-            <li ><a href="#about">CONTACT</a></li>
+            <li><a href="aboutus">ABOUT US</a></li>
+            <li><a href="foryou">FOR YOU</a></li>
+            <li><a href="services">SERVICES</a></li>
+            <li ><a href="blog">BLOG</a></li>
+            <li ><a href="vlog">VLOG</a></li>
+            <li ><a href="contact">CONTACT</a></li>
         </ul>
 
       
